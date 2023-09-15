3b0fb5e42c91e283ac1bb071b95ab1088b11fd96
najimRizky <najimajim1@gmail.com>
2023-09-15 22:31:22 +0700

    fix(provider): bug post provider

--- src/providers/post/index.ts
+++ src/providers/post/index.ts
@@ -5,15 +5,21 @@ import { getUserById } from "../user";
 import IUser from "@/interfaces/user";
 
 export const getPostList = async (page: number = 1) => {
-  const posts: any = await getRequest({ url: `${apiPath.posts}?pagess=${page}` })
+  const posts: any = await getRequest({ url: `${apiPath.posts}?page=${page}` })
   const uniqueUserIds: number[] = Array.from(new Set(posts?.data?.map((post: IPost) => post.user_id)));
 
   const userPromises = uniqueUserIds.map((id: number) => getUserById(id));
   const users: any = await Promise.all(userPromises);
 
   const postsWithUser = posts?.data.map((post: IPost) => {
-    const user: IUser = users?.data?.find((user: IUser) => user.id === post.user_id);
-    return { ...post, user };
+    const user: { data: IUser } = users?.find((user: { data: IUser | null }) => {
+      return user?.data?.id === post.user_id;
+    });
+
+    return {
+      ...post,
+      user: user?.data
+    }
   });
 
   return {
