87d1a2e9e2ed5e52fd1b7531d69b537d262ad438
najimRizky <najimajim1@gmail.com>
2023-09-15 08:39:09 +0700

    refactor(component): move path location

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,4 @@
-import SearchInput from "../components/pages/search/SearchInput";
+import SearchInput from "@/components/pages/search/SearchInput";
 
 interface SearchPageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -6,7 +6,6 @@ interface SearchPageProps {
 
 const SearchPage = ({ searchParams }: SearchPageProps) => {
   const data = searchParams
-  console.log(data)
   return (
     <div>
       <h1>SearchPage</h1>
