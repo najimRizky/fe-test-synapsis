67ce1fee342342a4ee3c407c248b9b2e07724dd6
najimRizky <najimajim1@gmail.com>
2023-09-16 10:09:35 +0700

    feat(user): add user page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,8 +1,37 @@
+import PageTitle from '@/components/modules/PageTitle';
+import Pagination from '@/components/modules/Pagination';
+import SectionTitle from '@/components/modules/SectionTitle';
+import UserCard from '@/components/modules/UserCard';
+import IUser from '@/interfaces/user';
+import { getUserList } from '@/providers/user'
 import React from 'react'
 
-const UserPage = () => {
+interface IUserPage {
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+const UserPage = async ({ searchParams }: IUserPage) => {
+  const page = Number(searchParams.page) || 1
+  const users = await getUserList(page)
+
   return (
-    <div>UserPage</div>
+    <div className="container">
+      <PageTitle
+        title="USERS"
+        description="See who's sharing their thoughts and stories with the world and connect with them."
+      />
+      <SectionTitle title={`Page ${page}`} />
+      <div className="grid grid-cols-4 gap-8">
+        {users.data.map((user: IUser) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+      <Pagination
+        page={page}
+        maxPage={users.metadata?.maxPage}
+        totalElement={users.metadata?.totalElement || 1}
+      />
+    </div>
   )
 }
 
