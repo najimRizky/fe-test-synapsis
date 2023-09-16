521b6d16b156346c2639981348d1f6b2c187bc2b
najimRizky <najimajim1@gmail.com>
2023-09-16 10:07:26 +0700

    refactor(user-card): create separate component

---
+++ src/components/modules/UserCard/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import IUserCard from './interface'
+import Link from 'next/link'
+import UserIcon from '@/components/icons/UserIcon'
+import MailIcon from '@/components/icons/MailIcon'
+
+const UserCard = ({ user }: IUserCard ) => {
+  return (
+    <Link
+      href={`/users/${user.id}`}
+      className="min-w-[14rem] h-32 shadow-sm cursor-pointer p-2 py-4 space-y-1 hover:shadow-lg duration-300"
+    >
+      <div className="flex gap-2 items-start font-bold  ">
+        <UserIcon width={22} />
+        <h3 className="overflow-ellipsis whitespace-nowrap overflow-hidden">{user.name}</h3>
+      </div>
+      <div className="flex gap-2 items-start">
+        <MailIcon width={22} />
+        <p className="overflow-ellipsis whitespace-nowrap overflow-hidden">{user.email}</p>
+      </div>
+      <p className={`text-sm capitalize font-bold`}>
+        {user.gender}
+      </p>
+      <p className={`text-sm capitalize font-bold ${user.status === 'active' ? 'text-green-500' : 'text-red-500'}`}>
+        {user.status}
+      </p>
+    </Link>
+  )
+}
+
+export default UserCard
