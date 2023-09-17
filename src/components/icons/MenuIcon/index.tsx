c2b00c65facdb0babda6bb1ffe16b10a64afcb93
najimRizky <najimajim1@gmail.com>
2023-09-17 17:54:02 +0700

    feat(header): implement responsive

---
+++ src/components/icons/MenuIcon/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MenuIcon = ({ width = 24, }) => {
+  return (
+    <svg width={width} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M5 7H19" stroke="#33363F" strokeWidth="2" strokeLinecap="round" />
+      <path d="M5 12H15" stroke="#33363F" strokeWidth="2" strokeLinecap="round" />
+      <path d="M5 17H11" stroke="#33363F" strokeWidth="2" strokeLinecap="round" />
+    </svg>
+
+  )
+}
+
+export default MenuIcon
--- src/components/modules/Header/index.tsx
+++ src/components/modules/Header/index.tsx
@@ -1,11 +1,26 @@
 "use client"
 
+import CloseIcon from "@/components/icons/CloseIcon"
+import MenuIcon from "@/components/icons/MenuIcon"
 import headerLinks, { IHeaderLink } from "@/config/headerLinks"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { useState } from "react"
 
 const Header = () => {
   const pathName = usePathname()
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
+
+  const toggleMenu = (state: boolean = !isMenuOpen) => {
+    setIsMenuOpen(state)
+
+    if (state) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }
+
   return (
     <header className="shadow-sm sticky top-0 bg-body-bg z-10">
       <div className="container">
@@ -21,7 +36,9 @@ const Header = () => {
               Synapsis Blog
             </div>
           </Link>
-          <div className="flex absolute left-[50%] top-0 h-full items-center translate-x-[-50%]">
+
+          {/* Desktop Menu */}
+          <div className="hidden sm:flex absolute left-[50%] top-0 h-full items-center translate-x-[-50%] ">
             {headerLinks.map((link: IHeaderLink) => (
               <Link
                 href={link.path}
@@ -36,9 +53,41 @@ const Header = () => {
               </Link>
             ))}
           </div>
-          <div>
-            About
+          <div className="text-sm font-bold px-4 h-full items-center hidden sm:block">
+            ABOUT
           </div>
+
+          {/* Mobile responsive menu */}
+          <div className="block sm:hidden cursor-pointer" onClick={() => toggleMenu(true)}>
+            <MenuIcon width={32} />
+          </div>
+          <div
+            className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 ${isMenuOpen ? 'block' : 'hidden'}`}
+            onClick={() => toggleMenu(false)}
+          >
+            {/* Mobile Menu */}
+            <div className="absolute top-0 right-0 h-full w-[16rem] bg-white p-4">
+              <div className="cursor-pointer w-fit ms-auto mb-4" onClick={() => toggleMenu(false)}>
+                <CloseIcon width={32} />
+              </div>
+              <div className="flex-col flex gap h-full">
+                {headerLinks.map((link: IHeaderLink) => (
+                  <Link
+                    href={link.path}
+                    key={link.path}
+                    className={`
+                      text-sm font-bold p-2 hover:bg-teal-100 duration-300 text-right
+                      ${pathName === link.path ? 'bg-teal-500 text-white hover:bg-teal-500' : ''}
+                    `}
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+
+          </div>
+
         </div>
       </div>
     </header>
