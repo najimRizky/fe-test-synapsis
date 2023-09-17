e8b5516582537709f5e7894ac06b827d2ebc119d
najimRizky <najimajim1@gmail.com>
2023-09-17 17:57:14 +0700

    feat(home): implement responsive

--- src/components/pages/home/Banner/index.tsx
+++ src/components/pages/home/Banner/index.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className="min-h-[18rem] py-4 flex justify-center items-center bg-teal-500 -mt-8 text-body-bg">
       <div className="container">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="text-6xl font-bold text-center">Welcome to S-BLOG</h1>
+          <h1 className="text-4xl md:text-6xl font-bold text-center">Welcome to S-BLOG</h1>
           <p className="text-lg text-center">
             Here you can find all the latest news and updates from the S-BLOG team.
           </p>
